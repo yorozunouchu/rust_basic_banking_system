50a5948811318b88e95691c5900d29cc53996c06
tadashim <ma.tadashi@gmail.com>
2024-01-01 14:44:08 +0900

    traits in functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+trait Printable {
+    fn print(&self);
+}
+
+fn print_something<T: Printable>(item: &T) {
+    item.print();
+}
+
+struct Book {
+    title: String,
+}
+
+impl Printable for Book {
+    fn print(&self) {
+        println!("Book: {}", self.title);
+    }
+}
+
+struct Magazine {
+    name: String,
+}
+
+impl Printable for Magazine {
+    fn print(&self) {
+        println!("Magazine: {}", self.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let book = Book {
+        title: String::from("Rust Programming Language"),
+    };
+    let magazine = Magazine {
+        name: String::from("The New Yorker"),
+    };
+
+    print_something(&book);
+    print_something(&magazine);
 }
